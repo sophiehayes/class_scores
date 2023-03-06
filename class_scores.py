b1a7f25d61752c39fe6b305f4abf2dc45b3d1f24
sophiehayes <126836278+sophiehayes@users.noreply.github.com>
2023-03-07 11:36:33 +1300

    finished 2 and 3

--- class_scores.py
+++ class_scores.py
@@ -3,12 +3,27 @@
 # sophie the lord
 # 02/03/2023
 
+# funtion for input
+def user_input (user_score):
+    '''
+    user input to list
+    '''
+    combined_list.append(user_score)
+
+    return user_score
+        
+    
 # lits
 class_a = [44, 33, 28, 47, 12, 28, 32, 31, 11, 39, 40, 26, 36]
 class_b = [19, 26, 38, 31, 30, 42, 44, 14, 27, 43, 46, 49, 24, 26, 36]
 
 # combine list
 combined_list = class_a + class_b
+
+
+# get user to add score and calc inside funtion
+user_score = float(input("what score did you get? "))
+user_input(user_score)
 
 # set variables
 count = 0
@@ -18,6 +33,7 @@ total_score = 0
 for classes in combined_list:
     count += 1
     total_score += classes
+
     
 # print mean
 print("the mean is {:.2f} for both class lists combined".format(total_score/count))
@@ -27,4 +43,6 @@ print("the max score for both class lists combined is: ", max(combined_list))
 
 # print min
 print("the min score for both class lists combined is: ", min(combined_list))
+
+
 
